2a0dd5381653b0f0e065e6c175a0a41761a341b9
Dennis Castillo <dennis.castillo@sharp.com>
2023-02-04 11:54:06 -0800

    updated budgets

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 #create resources
+
+data "azurerm_subscriptions" "current" {}
 module "compute" {
   source  = "app.terraform.io/denniscastillo/compute/azure"
   #source = "github.com/casdeone/terraform-azure-compute.git?ref=v.1.1.0"
@@ -10,11 +12,17 @@ module "compute" {
   vm_size = "Standard_DS1_v2"
 }
 
+// create resource group
+resource azurerm_resource_group "dev_rg" {
+  name = "dev-rg"
+  location = "WestUS3"
+}
 module "dev-budgets" {
   source  = "app.terraform.io/denniscastillo/dev-budgets/azure"
-  version = "1.0.6"
-  location = "westus3"
-  resource_group_name = "rg-test"
+  version = "1.0.7"
+  location = azurerm_resource_group.dev_rg.location
+  resource_group_name = azurerm_resource_group.dev_rg.name
   email_contact = ["casdeone@gmail.com"]
-
+  subscription_name = data.azurerm_subscriptions.current.display_name_prefix
+  subscription_id = data.azurerm_subscriptions.current.id
 }
